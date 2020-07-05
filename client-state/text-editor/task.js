df123961024fb4259266d7517ce34802f34db132
Andrey Khomutov <homutovan@live.com>
2020-07-05 20:20:40 +0700

    fix bug

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -2,7 +2,7 @@
 
 let editor = document.getElementById('editor');
 let button = document.querySelector('.button');
-editor.value = window.localStorage.text;
+window.localStorage.text ? editor.value = window.localStorage.text : '';
 
 editor.addEventListener('input', () => window.localStorage.text = editor.value);
 button.addEventListener('click', () => editor.value = window.localStorage.text = '');
